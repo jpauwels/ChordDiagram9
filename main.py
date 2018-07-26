f39b85633d7bf63ad6a6637a5895337e4b6db79a
Will Ginsberg <will.j.ginsberg@gmail.com>
2018-07-26 11:18:22 -0400

    Add audio

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import json
 
 app = Flask(__name__)
 
+AUDIO_DIR = "audio"
 TRACK_DIR = "tracks"
 tracks = os.listdir(TRACK_DIR)
 
@@ -29,8 +30,8 @@ def process_track(path):
     
     # Adds chords
     for start, end in changes:
-        matrix[start][end] = 0.25 
-        matrix[end][start] = 0.1
+        matrix[start][end] = 0.5 
+        matrix[end][start] = 0.25
 
     # Gives each arc the same width
     for i in range(len(matrix)):
@@ -46,6 +47,10 @@ def default_matrix():
     for i in range(len(matrix)):
         matrix[i][i] = 1 - sum(matrix[i])
     return matrix 
+
+def get_audio_path(name):
+    wav = name[:-5] + ".wav"
+    return os.path.join(AUDIO_DIR, wav)
 
 @app.route("/")
 def template_test():
@@ -63,9 +68,17 @@ def get_track(name):
     Page for each song
     '''
     return render_template('template.html',
+                            audio_file=get_audio_path(name),
                             track_list=tracks,
                             chord_map=CHORDS,
                             matrix=process_track(os.path.join(TRACK_DIR, name)))
+
+@app.route("/tracks/audio/<name>")
+def get_audio(name):
+    '''
+    Audio for each song
+    '''
+    return send_from_directory("audio", name)
 
 if __name__ == '__main__':
     app.run(debug=True)
